aff4e76f38b7400290769a8b76986a4fe18daff9
lukkmanov <78071615+lukkmanov@users.noreply.github.com>
2021-02-04 21:05:55 +0500

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int a,b=21;
-    int mi,ma;
+    int a, b = 21;
+    int mi, ma;
     cout << "Enter the number" << endl;
     cin >> a;
-    mi = (a < b == 1)  ;
-    ma = (a > b == 1)  ;
-    cout << mi << endl;
+    mi = (a < b == 0) + (b-a) ;
+    ma = (a > b == 0) + (a * 2 ) ;
+    cout << mi << endl;    cout << ma << endl;
     return 0;
 }
